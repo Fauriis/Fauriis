9cdbded4a05583624b047e6f89c9caebc5fd14e6
Fauriis <dobreflorentina58@gmail.com>
2022-12-11 19:07:03 +0200

    project

---
+++ react-carousels/src/App.tsx
@@ -0,0 +1,8 @@
+import { Carousel } from './carousel/Carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+
+function App() {
+  return <Carousel></Carousel>;
+}
+
+export default App;
